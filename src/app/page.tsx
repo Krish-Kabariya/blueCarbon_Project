eedd0d31d17e1ee5c19b4ba982766d9af4eb3b60
Dakshit Jambukiya <jambukiyadaxit2301@gmail.com>
2025-08-30 19:40:21 +0000

    change image of Our Mission: A Resilient Coast for a Thriving Planet sec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,12 +117,12 @@ export default function Home() {
             </div>
             <div className="flex justify-center">
               <Image
-                src="https://picsum.photos/600/400?random=11"
-                alt="Mangrove Forest"
+                src="https://picsum.photos/600/400?random=12"
+                alt="Seagrass Bed"
                 width={600}
                 height={400}
                 className="overflow-hidden rounded-xl object-cover"
-                data-ai-hint="mangrove forest"
+                data-ai-hint="seagrass bed"
               />
             </div>
           </div>
